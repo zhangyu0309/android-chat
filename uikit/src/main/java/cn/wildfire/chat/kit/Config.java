c8f2d470996d9bc4462a908532f8cd31531925a6
zhangyu0309 <zhangyu0309@yeah.net>
2024-02-20 17:34:48 +0800

    Update Config.java

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -35,7 +35,7 @@ public class Config {
         // 如果是高级版，请删除掉下面的配置项目，保持ICE_SERVERS为空数组就行。
         // 数组元素定义
         /*{"turn server uri", "userName", "password"}*/
-        {"turn:turn.wildfirechat.net:3478", "wfchat", "wfchat1"}
+        {"turn:101.200.232.7:3478", "iturn", "Inspu9961234,"}
     };
 
     //文件传输助手用户ID，服务器有个默认文件助手的机器人，如果修改它的ID，需要客户端和服务器数据库同步修改
